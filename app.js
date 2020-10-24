b27d2d58749bd5664758adf7b8ae3aaf5a05cf5a
LeeChan <lee79102@hotmail.com>
2020-10-24 23:38:34 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+// 引用資料庫
+require('./config/mongoose')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-// 設定 port
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
+// 設定 PORT
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+// config/mongoose.js
+// 如果在 Heroku 環境則使用 process.env.MONGODB_URI
+// 否則為本地環境，使用 mongodb://localhost/todo-list
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+module.exports = db
