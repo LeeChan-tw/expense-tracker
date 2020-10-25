f6c557d316ed37ea534dcba8f872c2e87c5f8369
LeeChan <lee79102@hotmail.com>
2020-10-25 22:58:08 +0800

    feat: add sorting function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,11 +11,37 @@ router.get('/', (req, res) => {
     .lean()
     .sort({ date: 'asc' })
     .then(records => {
+      let sum = 0
+      for (const record of records) {
+        sum += record.amount
+      }
       Category.find()
         .lean()
-        .then(categories => res.render('index', { records, categories }))
+        .then(categories => res.render('index', { sum, records, categories }))
     })
     .catch(error => console.error(error))
+})
+// 篩選支出路由
+router.get('/sort', (req, res) => {
+  const { category } = req.params
+  const { sort } = req.query
+  if (sort === 'all') {
+    res.redirect('/')
+  } else {
+    Record.find({ category: sort })
+      .lean()
+      .sort(sort)
+      .then(records => {
+        let sum = 0
+        for (const record of records) {
+          sum += record.amount
+        }
+        Category.find()
+          .lean()
+          .then(categories => res.render('index', { categories, records, sort, category, sum }))
+      })
+      .catch(error => console.log(error))
+  }
 })
 
 // 匯出路由模組
